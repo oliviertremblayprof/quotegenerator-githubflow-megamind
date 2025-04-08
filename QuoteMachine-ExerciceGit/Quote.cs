fc6e6ecaad656fc635fb080c9bd8008e26b907be
EduFelixDallaire <fedal001@edu.cegepgarneau.ca>
2025-04-08 13:44:53 -0400

    test check

--- QuoteMachine-ExerciceGit/Quote.cs
+++ QuoteMachine-ExerciceGit/Quote.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QuoteMachine_ExerciceGit
+﻿namespace QuoteMachine_ExerciceGit
 {
     public class Quote
     {
-        public string Text { get; set; }
-        public string Author { get; set; }
+        public required string Text { get; set; }
+        public required string Author { get; set; }
 
         public override string ToString()
         {
